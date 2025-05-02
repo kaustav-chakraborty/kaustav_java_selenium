4f0ec4ac01b1557b91c6c24d543d222b484280d2
Kaustav Chakraborty <kaustavchakraborty99@gmail.com>
2025-05-03 00:09:47 +0530

    >commiting the changes:wq! :wq!  # :wq!  #

--- src/main/java/Constants/New_java_class.java
+++ src/main/java/Constants/New_java_class.java
@@ -3,5 +3,35 @@ package Constants;
 public class New_java_class {
     public static void main(String[] args){
         System.out.println("new class");
+        Integer [] rno=new Integer[5];
+        String[] name=new String[5];
+        Float [] marks=new Float[5];
+        Student kaustav=new Student();
+        Student random=new Student();
+        kaustav.marks=55.5f;
+        kaustav.name="kaustav";
+        System.out.println(kaustav.marks);
+        System.out.println(kaustav.name);
+    }
+}
+
+class Student{
+    float marks=90f;
+    String name;
+    int rno;
+    void Student1(){
+        this.rno=13;
+        this.marks=88.5f;
+        this.name="kaustav";
+    }
+    Student(int roll,String name,float marks){
+        this.rno=roll;
+        this.name=name;
+        this.marks=marks;
+
+    }
+    //this is how you call a constructor from another constructor
+    Student(){
+        this(19,"kaustav 2",99.5f);
     }
 }
